6ec48485b1b3091355952c911fccd01653a9e7d1
Javier Rodriguez <xjavierx1995@gmail.com>
2024-01-07 09:18:20 -0400

    prime vue

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -10,5 +10,14 @@ import router from './router';
 //Pinia
 import pinia from './plugins/pinia';
 
+//PrimeVue
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/lara-light-green/theme.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
-createApp(App).use(pinia).use(router).mount("#app");
+createApp(App)
+  .use(pinia)
+  .use(router)
+  .use(PrimeVue)
+  .mount("#app");
--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,10 +1,25 @@
 <template>
   How To Install Vue 3 in Laravel 9 with Vite - TechvBlog
 
-  <button type="button" @click="goFavorites">Ir a favoritos</button>
+
+
+
+  <div class="flex flex-column md:flex-row md:justify-content-between row-gap-3">
+      <Button type="button" label="Button 1"></Button>
+      <Button type="button" label="Button 2" severity="secondary"></Button>
+      <Button type="button" label="Button 3" severity="help"></Button>
+  </div>
+
+
+<i class="pi pi-check"></i>
+<i class="pi pi-times"></i>
+<span class="pi pi-search"></span>
+<span class="pi pi-user"></span>
+  <Button @click="goFavorites" label="Ir a favoritos"></Button>
 </template>
 <script lang="ts" setup>
 import router from '../router';
+import Button from "primevue/button"
 
 
   function goFavorites() {
